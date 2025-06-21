28fd310e1444a26f4f971d491d0ee989a98b81b7
manishyadav-10 <my5170626@gmail.com>
2025-06-21 13:30:44 +0530

    Update postcss.config.js

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,16 +1,8 @@
-// export default {
-//   plugins: {
-//     tailwindcss: {},
-//     autoprefixer: {},
-//   },
-// }
-import tailwindcss from '@tailwindcss/postcss';
-import autoprefixer from 'autoprefixer';
-
 export default {
-  plugins: [
-    tailwindcss,
-    autoprefixer
-  ],
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+
 
