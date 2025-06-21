d10c6b485c756d446546cdfc19060c536db87473
manishyadav-10 <my5170626@gmail.com>
2025-06-21 14:21:30 +0530

    Update ProfilePage.jsx

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -46,10 +46,10 @@ const handleSubmit = async (e)=>{
             Upload profile image
           </label>
           <input onChange={(e)=>setName(e.target.value)} value={name}
-          type="text" required placeholder='Your Name' className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2
+          type="text" required placeholder='Your Name' className='p-2 border border-gray-500 rounded-md bg-gray-800 text-white placeholder-gray-400  focus:outline-none focus:ring-2
           focus:ring-violet-500' />
           <textarea onChange={(e)=>setBio(e.target.value)} value={bio}
-           placeholder='Write profile bio' required className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2
+           placeholder='Write profile bio' required className='p-2 border border-gray-500 rounded-md bg-gray-800 text-white placeholder-gray-400  focus:outline-none focus:ring-2
           focus:ring-violet-500' rows={4}></textarea>
           <button type='submit' className='bg-gradient-to-r from-purple-400
           to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer'>
